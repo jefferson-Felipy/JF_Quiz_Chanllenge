f03fd265df6a21b4517f328d05b9a1262cd396dc
jefferson-Felipy <jeffersonthm1@gmail.com>
2023-10-14 14:28:02 -0300

    change: inserção de novas perguntas

--- src/Categorias.js
+++ src/Categorias.js
@@ -1,7 +1,7 @@
 // Array para armazenar objetos de categoria
 const categorias = [];
 export default categorias;
-
+//1
 categorias.push({
     nome: 'Geral',
     texto: 'Que historia em quadrinhos tem um gato que adora lasanha?',
@@ -9,7 +9,7 @@ categorias.push({
     resposta: 'Garfield',
     opcoes: ['Snoopy', 'Frajola e Piupiu', 'Manda-Chuva','Garfield'],
 });
-
+//2
 categorias.push({
     nome: 'Geral',
     texto: 'Qual detes animais não é um mamifero?',
@@ -17,15 +17,15 @@ categorias.push({
     resposta: 'Avestruz',
     opcoes: ['Avestruz', 'Ovelha', 'Vaca','Coelho'],
 });
-
+//3
 categorias.push({
     nome: 'Geral',
-    texto: 'Em qual estilo misical Duke Elington se especializou?',
+    texto: 'Em qual estilo musical Duke Elington se especializou?',
     status: '',
     resposta: 'Jazz',
     opcoes: ['Rock', 'Jazz', 'Country','Blues'],
 });
-
+//4
 categorias.push({
     nome: 'Geral',
     texto: 'Onde fica o paraguai?',
@@ -33,11 +33,51 @@ categorias.push({
     resposta: 'América do Sul',
     opcoes: ['América do Norte', 'Ásia', 'América do Sul','América Central'],
 });
-
+//5
 categorias.push({
     nome: 'Geral',
     texto: 'Em que esporte se destacou Michael Jordan?',
     status: '',
     resposta: 'Basquetebol',
     opcoes: ['Rally', 'Basquetebol', 'Handebol','Futebol'],
+});
+//6
+categorias.push({
+    nome: 'Geral',
+    texto: 'A vida de Clarice Lispector sempre foi muito intensa. Qual dos epsódios abaixo marcou a carreira da escritora no final dos anos 60?',
+    status: '',
+    resposta: 'Um incendio em seu apartamento',
+    opcoes: ['A perda do filho mais velho', 'Um incendio em seu apartamento', 'Um acidente de automóvel','A perda do filho mais novo'],
+});
+//7
+categorias.push({
+    nome: 'Geral',
+    texto: 'Qual a capital do ceará?',
+    status: '',
+    resposta: 'Fortaleza',
+    opcoes: ['Fortaleza', 'Teresina', 'Rio Branco','Natal'],
+});
+//8
+categorias.push({
+    nome: 'Geral',
+    texto: 'De qual cidade eram os Beatles?',
+    status: '',
+    resposta: 'Liverpool',
+    opcoes: ['Londres', 'Mancherster', 'Liverpool','Dublin'],
+});
+//9
+categorias.push({
+    nome: 'Geral',
+    texto: 'Que cidade foi anfitriã dos jogos olímpicos de 1928?',
+    status: '',
+    resposta: 'Amsterdã',
+    opcoes: ['Berlin', 'Paris', 'Amsterdã','Rio de Janeiro'],
+});
+//10
+categorias.push({
+    nome: 'Geral',
+    texto: 'Qual é o nome que denomina um tipo de alimento livre de agrotóxicos e que é normalmente cultivado em pequenas quantidades?',
+    status: '',
+    resposta: 'Orgánicos',
+    opcoes: ['Orgánicos', 'Calóricos', 'Transgenicos','Hidropônicos'],
 });
