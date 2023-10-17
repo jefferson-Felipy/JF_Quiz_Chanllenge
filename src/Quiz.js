940eb41895b84a710b1e66e065a48e7482f85098
jefferson-Felipy <jeffersonthm1@gmail.com>
2023-10-17 16:13:49 -0300

    change: atualização dos comandos de interação do Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,32 +1,39 @@
+"use strict";
+
 //Importando as Categorias_
 import categorias from "./Categorias.js";
 
 const p_cat = document.querySelector('#p_cat');
 const p_text = document.querySelector('#p_text');
+
 const btn_next = document.querySelector('#btn_next');
 const buttons = document.querySelector('.buttons');
+
 const cronometry = document.querySelector('#cronometry');
+const p_points = document.querySelector('#p_points');
+
+const hide = document.querySelector('#hide');
+const Error_field = document.querySelector('#Error_field');
+const right_answer = document.querySelector('#right_answer');
+
 let list_numbers = [];
 let next_valid = false;
 let segundos = 0;
+let points = 0;
 
 class Quiz{
     contructor(){
         this.randon = 0;
         this.list_options = [];
-        this.list_letters = [];
         this.interval = null;
-        this.seconds = 0;
     }
 
     Number_Randon = () =>{
 
         //Configuracao para fins de nao repetir as mesmas perguntas_
         //Redefinindo o array de indices usados caso todos ja forem usados_
-        if(list_numbers.length === categorias.length){//Esse if precisa vim antes do loop;
-            list_numbers = [];
-            console.log('recomecou!');
-        }
+        if(list_numbers.length === categorias.length) list_numbers = [];//Esse if precisa vim antes do loop;
+
         //Usando o loop para verificar se o indice sorteado ja nao tenha sido usado_
         do{
             this.randon = Math.floor(Math.random()*categorias.length);
@@ -35,9 +42,11 @@ class Quiz{
             se nao haver, retorna false e sai do loop, e assim sucessivamente;*/
         }while(list_numbers.includes(this.randon)); 
 
-        this.list_letters = ["A","B","C","D"];
+        //Preenchendo os arrays de acordo com suas especificações_
         this.list_options = [...categorias[this.randon].opcoes];
         list_numbers.push(this.randon);
+
+        //Botão de proxima pergunta inicialmente invisível_
         btn_next.style.opacity = '0';
     }
 
@@ -56,6 +65,7 @@ class Quiz{
             button.id = 'btns';
             //Inserindo os textos dos botões_
             button.textContent = `${this.list_options[n]}`;
+
             //Chamando o método Select_option() e passando um valor para ele_
             button.addEventListener('click',() => {
                 //Chamando o metodo Select_option e passando valores desse para ele_
@@ -74,11 +84,16 @@ class Quiz{
 
         //Exibe a opção correta após 3 segundos_
         this.interval = setTimeout(() => {
-            this.Stop_Cronometry();
-            next_valid = true;
-            btn_next.style.opacity = '1';
+            this.Stop_Cronometry();//Chamando o cronometro;
 
-            if(value == res) button.style.backgroundColor = '#32CD32';
+            //verificando e exibindo as respostas_
+            if(value == res){
+                next_valid = true;//true indicando que um botão foi clicado;
+                btn_next.style.opacity = '1';//Exibindo o botão de próxima pergunta;
+                button.style.backgroundColor = '#32CD32';
+                points += 10;
+                p_points.textContent = points;
+            }
             else{
                 //Formatando após clicar na errada_
                 button.style.backgroundColor = 'red';
@@ -86,6 +101,8 @@ class Quiz{
                 for(let n = 0;n < 4;n++){
                     if(allButtons[n].textContent == res) allButtons[n].style.backgroundColor = '#32CD32';
                 }
+
+                this.Error_field();
             } 
         },3000);
 
@@ -93,7 +110,6 @@ class Quiz{
         allButtons.forEach((btn) => btn.setAttribute('disabled', true));
     }
 
-    //Cronometro para exibir a resposta_
     Start_Cronometry = () => {
         cronometry.textContent = '';
         this.interval = setInterval(() => {
@@ -110,6 +126,15 @@ class Quiz{
     Reset_Cronometry = () => {
         segundos = 0;
         cronometry.textContent = '';
+    }
+
+    Error_field = () => {
+        //Exibindo resposta correta_
+        right_answer.textContent += categorias[this.randon].resposta;
+        hide.classList.add('hide');
+        Error_field.classList.add('Error_field');
+        hide.style.opacity = '1';
+        Error_field.style.opacity = '1';
     }
 
     Clean_Group = () => {
